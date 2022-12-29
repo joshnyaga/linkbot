07ccf5ffa636727e01818395d21bbb60a99a7863
Ubuntu <ubuntu@ip-172-31-92-40.ec2.internal>
2022-12-29 08:01:38 +0000

    made some changes on the link detection

--- app.js
+++ app.js
@@ -18,21 +18,29 @@ app.get('/', function (req, res) {
     res.send('Hello World');
  })
 client.on("message", async(message) => {
-    if (message.body) {
-        console.log(message.body);
-    }
-    const code = message.body.slice(33,56);
-    const link = message.body.slice(0,33);
+  if(message.body.includes("chat.whatsapp")){
+	console.log("Link Detected")
+    let position = message.body.search("chat.whatsapp.com/");
+
+let codestart = position+18;
+    let code = message.body.slice(codestart,codestart+29);
+if(code.includes("invite")){
+	console.log("contains invite")
+	code = code.slice(7,36);
+}
+    let link = message.body.slice(0,33);
     console.log(code)
-    if(link === "https://chat.whatsapp.com/invite/"){
-    try {
+   try {
         console.log("link is == "+message.body)
         await client.acceptInvite(code);
+	console.log("group joined")
       } catch (e) {
+	console.log("an error occurred when joining group");
         console.log(e)
      
     }
-    }
+    
+}
 //   if (message.body) {
 //     console.log(message.body);
     
